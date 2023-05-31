fb48e3704a58f60a2b6a482f76083de786f8a9cd
BadPetrovich <ivantusx@gmail.com>
2023-05-31 15:40:48 +0300

    Initial commit

---
+++ src/main/java/com/badpetrovich/divinationforlove_mvc/DivinationforloveMvcApplication.java
@@ -0,0 +1,13 @@
+package com.badpetrovich.divinationforlove_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivinationforloveMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivinationforloveMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/badpetrovich/divinationforlove_mvc/DivinationforloveMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.badpetrovich.divinationforlove_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivinationforloveMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
