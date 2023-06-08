6360783d2030b2a4c7a2e7f89c79db0467984748
BadPetrovich <ivantusx@gmail.com>
2023-06-08 13:54:30 +0300

    Продолжаем разбираться с созданием объектов через html

--- src/main/java/com/badpetrovich/divinationforlove_mvc/controller/MyController.java
+++ src/main/java/com/badpetrovich/divinationforlove_mvc/controller/MyController.java
@@ -1,23 +1,52 @@
 package com.badpetrovich.divinationforlove_mvc.controller;
 
+import com.badpetrovich.divinationforlove_mvc.model.Lover;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/dfl")
 public class MyController {
 
-    @GetMapping("/")
+    @GetMapping("/first")
     public String welcomePage(){
         return "first_page";
     }
     @GetMapping("/starting")
-    public String startingPage(){
+    public String startingPage(Model model){
+
+        model.addAttribute("firstLover", new Lover());
+        model.addAttribute("secondLover", new Lover());
+        model.addAttribute("thirdLover", new Lover());
+        model.addAttribute("fourthLover", new Lover());
+
         return "starting_page";
     }
 
+//    @GetMapping("/game")
+//    public String gamePage(HttpServletRequest request, Model model){
+//
+//        String loverName = request.getParameter("firstName");
+//        model.addAttribute("firstName", loverName);
+//        loverName = request.getParameter("secondName");
+//        model.addAttribute("secondName", loverName);
+//        loverName = request.getParameter("thirdName");
+//        model.addAttribute("thirdName", loverName);
+//        loverName = request.getParameter("fourthName");
+//        model.addAttribute("fourthName", loverName);
+//        return "game_page";
+//    }
+
     @GetMapping("/game")
-    public String gamePage(){
+    public String gamePage(@ModelAttribute("firstLover") Lover firstLover,
+                           @ModelAttribute("secondLover") Lover secondLover,
+                           @ModelAttribute("thirdLover") Lover thirdLover,
+                           @ModelAttribute("fourthLover") Lover fourthLover){
+
         return "game_page";
     }
 }
---
+++ src/main/java/com/badpetrovich/divinationforlove_mvc/model/Lover.java
@@ -0,0 +1,89 @@
+package com.badpetrovich.divinationforlove_mvc.model;
+
+import java.util.*;
+
+public class Lover {
+    private String name;
+
+    private List<String> myDeck = new ArrayList<>();
+
+    public Lover() {
+    }
+
+    public void getCardToBuffer(int boundOfRandom, List<String> cards){
+
+        Random random = new Random();
+        int randomCard;
+
+        for(int i = 0; i<8; i++){
+            randomCard = random.nextInt(boundOfRandom);
+            myDeck.add(cards.get(randomCard));
+            cards.remove(randomCard);
+            boundOfRandom--;
+        }
+        myDeck.sort(new CardComparator());
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getMyDeck() {
+        return myDeck;
+    }
+
+    public void clearMyDeck() {
+       myDeck.removeAll(myDeck);
+    }
+
+    @Override
+    public String toString() {
+        return "Lover{" +
+                "name='" + name + '\'' +
+                ", myDeck=" + myDeck +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Lover lover = (Lover) o;
+        return Objects.equals(name, lover.name) && Objects.equals(myDeck, lover.myDeck);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, myDeck);
+    }
+
+
+}
+
+class CardComparator implements Comparator<String>{
+    String[] suit = {"spades", "clubs", "diamonds", "hearts"};
+
+    @Override
+    public int compare(String o1, String o2) {
+        String[] suit1 = o1.split(" ");
+        String[] suit2 = o2.split(" ");
+
+        int result = 0;
+        for (String s : suit) {
+            if (suit1[1].matches(s) && suit2[1].matches(s)) {
+                break;
+            } else if (suit1[1].matches(s) && !suit2[1].matches(s)) {
+                result = 1;
+                break;
+            } else if (!suit1[1].matches(s) && suit2[1].matches(s)) {
+                result = -1;
+                break;
+            }
+        }
+        return result;
+    }
+}
